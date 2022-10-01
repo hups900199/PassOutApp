16b3411ba27bbf66a941432dc81b2e88d38ae34b
Jia Hong Ye <hups900199@gmail.com>
2022-10-02 06:47:57 +0800

    Create sign out button function.

--- app/src/main/java/com/example/passoutapp/MainActivity.kt
+++ app/src/main/java/com/example/passoutapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.passoutapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.passoutapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.signoutbtn.setOnClickListener {
+            firebaseAuth.signOut()
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
